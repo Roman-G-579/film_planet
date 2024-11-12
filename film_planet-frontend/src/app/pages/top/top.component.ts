d34227d705d35cd57df26303e1401578410d2381
Roman-G-579 <romangury579@gmail.com>
2024-11-12 19:50:26 +0200

    added initial top films/tv component

---
+++ film_planet-frontend/src/app/pages/top/top.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './top.component.html',
+  styleUrl: './top.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TopComponent {
+
+}
