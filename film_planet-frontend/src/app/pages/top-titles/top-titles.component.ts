8bb990568656c4cf4a624f45a7c65ddb1043ce84
Roman-G-579 <romangury579@gmail.com>
2024-11-19 20:12:25 +0200

    Updated top-titles component functionality

--- film_planet-frontend/src/app/pages/top-titles/top-titles.component.ts
+++ film_planet-frontend/src/app/pages/top-titles/top-titles.component.ts
@@ -3,13 +3,22 @@ import {TableModule} from 'primeng/table';
 import {ButtonModule} from 'primeng/button';
 import {LibraryItem} from '../../core/interfaces/library-item.interface';
 import {TopTitlesService} from './top-titles.service';
+import {IftaLabelModule} from 'primeng/iftalabel';
+import {Select} from 'primeng/select';
+import {FormsModule} from '@angular/forms';
+import {MediaType} from '../../core/enums/media-type.enum';
+import {CalendarModule} from 'primeng/calendar';
 
 @Component({
   selector: 'app-top-titles',
   standalone: true,
   imports: [
     TableModule,
-    ButtonModule
+    ButtonModule,
+    IftaLabelModule,
+    Select,
+    FormsModule,
+    CalendarModule
   ],
   templateUrl: './top-titles.component.html',
   styleUrl: './top-titles.component.scss',
@@ -18,9 +27,46 @@ import {TopTitlesService} from './top-titles.service';
 export class TopTitlesComponent implements OnInit {
   protected readonly topService = inject(TopTitlesService);
 
-  topFilms: WritableSignal<LibraryItem[]> = signal<LibraryItem[]>([]);
+  topTitles: WritableSignal<LibraryItem[]> = signal<LibraryItem[]>([]);
+  resultCriteria: WritableSignal<string> = signal("");
+
+  mediaTypes = Object.keys(MediaType).filter((item) => {
+    return isNaN(Number(item));
+  });
+  selectedMediaType: MediaType | undefined;
+
+  minYear: Date | undefined;
+  maxYear: Date | undefined;
+  selectedYear: number | undefined;
 
   ngOnInit() {
-    this.topFilms.set(this.topService.getTopOfAllTime());
+    this.topTitles.set(this.topService.getTopFilmsOfAllTime());
+    this.resultCriteria.set(this.topService.resultCriteria());
+
+    this.setMinAndMaxYears();
+    console.log(this.mediaTypes)
   }
+
+  setMinAndMaxYears() {
+    const currentYear = new Date().getFullYear();
+
+    this.minYear = new Date();
+    this.minYear.setFullYear(1900);
+    this.maxYear = new Date();
+    this.maxYear.setFullYear(currentYear);
+  }
+
+  filterByMediaType(type: MediaType | undefined) {
+    if (type) {
+      this.topTitles.set(this.topService.filterByMediaType(type));
+    }
+  }
+
+  filterByYear(year: number | undefined) {
+    console.log("function called");
+    if (year) {
+      this.topTitles.set(this.topService.filterByYear(year));
+    }
+  }
+
 }
