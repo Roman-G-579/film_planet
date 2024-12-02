9e3c2b0c400439eed6950a45573517409b15a713
Roman-G-579 <romangury579@gmail.com>
2024-12-02 19:19:43 +0200

    updated getGenreNamesFromIds function

--- film_planet-frontend/src/app/core/utils/data.utils.ts
+++ film_planet-frontend/src/app/core/utils/data.utils.ts
@@ -5,23 +5,26 @@ import {TvGenres} from '../constants/tv-genres.record';
 export class DataUtils {
 
   /**
-   * Converts genre ids to their corresponding names based on the id array and the media type
+   * Converts genre ids of the given media type to their corresponding names based on the id array and the media type
+   * Will convert a subset of genre ids if a genres array is provided
    * @param mediaType The given media type: Film or TV
+   * @param genres <i>(optional)</i> An array of specific genre numbers
    * @returns all genre names as an array of strings
    */
-  static getGenreNamesFromIds(mediaType: MediaType): string[] {
-    const genreNames: string[] = [];
+  static getGenreNamesFromIds(mediaType: MediaType, genres?: number[]): string[] {
+    const genreNames: string[] | string = [];
 
     // The genre object corresponds to the given media type
     const selectedObject: Record<string,number> = mediaType === MediaType.Film ? FilmGenres : TvGenres;
-    const genreIds = Object.values(selectedObject);
+
+    const genreIds = genres ? genres : Object.values(selectedObject);
+
     for (const id of genreIds) {
       const name = Object.keys(selectedObject).find((key) => selectedObject[key] === id);
       if (name) {
         genreNames.push(name);
       }
     }
-
     return genreNames;
   }
 
@@ -29,25 +32,10 @@ export class DataUtils {
     const genresObject: Record<string, number> = mediaType === MediaType.Film ? FilmGenres : TvGenres;
     return Object.keys(genresObject).find((key) => genresObject[key] === genreId) || '';
   }
+
   static getGenreIdFromName(genreName: string, mediaType: MediaType): number {
     const genresObject: Record<string, number> = mediaType === MediaType.Film ? FilmGenres : TvGenres;
     return genresObject[genreName];
   }
 
-  // /**
-  //  * Sanitizes the given string for use in webpage urls.
-  //  * -Removes leading and trailing whitespaces
-  //  * -Converts the string to lowercase
-  //  * -Replaces spaces with hyphens
-  //  * -Removes special characters
-  //  * @param text the given string
-  //  */
-  // static sanitizeString(text: string): string {
-  //   return text
-  //     .trim()
-  //     .toLowerCase()
-  //     .replace(/[\s]+/g, '-')
-  //     .replace(/[^a-z0-9-]/g, '')
-  //     .replace(/-+/g, '-'); // Replace multiple hyphens with a single one
-  // }
 }
