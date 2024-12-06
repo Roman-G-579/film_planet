18854e4eae594c917d02771968341ac34df42bea
Roman-G-579 <romangury579@gmail.com>
2024-12-06 14:22:05 +0200

    Added episodes value to season interface

--- film_planet-frontend/src/app/core/interfaces/season.interface.ts
+++ film_planet-frontend/src/app/core/interfaces/season.interface.ts
@@ -1,6 +1,9 @@
+import {Episode} from './episode.interface';
+
 export interface Season {
   series_id: number;
   id: number;
   number: number; // season 0 reserved for specials
   name?: string; // series 1 etc. for uk shows, miniseries for miniseries, pilot for individual pilots
+  episodes?: Episode[]; // Array of all episodes belonging to the season
 }
