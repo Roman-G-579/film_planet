db0bac044825731d75ce6442a338a2a43a941cba
Roman-G-579 <romangury579@gmail.com>
2024-12-07 12:38:52 +0200

    Added pipe to convert numerical values to hours and minutes values

---
+++ film_planet-frontend/src/app/core/pipes/minutes-to-hours.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'minutesToHours',
+  standalone: true
+})
+export class MinutesToHoursPipe implements PipeTransform {
+
+  transform(minutes: number): string {
+    if (minutes < 60) {
+      return `${minutes}m`;
+    }
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return remainingMinutes > 0 ? `${hours}h ${remainingMinutes}m` : `${hours}h`;
+  }
+
+}
