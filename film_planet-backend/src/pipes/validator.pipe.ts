36fdb5d3b649478848fe8cdfa75aed363ef6327e
Roman-G-579 <romangury579@gmail.com>
2024-12-11 20:32:13 +0200

    Added validators

---
+++ film_planet-backend/src/pipes/validator.pipe.ts
@@ -0,0 +1,11 @@
+import { param, body } from 'express-validator';
+
+export const validateMongoId = [param('id').exists().bail().isMongoId()];
+
+export const validateUser = [
+    body('username').exists().isString().notEmpty(),
+    body('password').exists().isString().notEmpty(),
+    body('firstName').exists().isString().notEmpty(),
+    body('lastName').exists().isString().notEmpty(),
+    body('email').exists().isEmail(),
+];
