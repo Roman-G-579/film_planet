4cfd7c46b5e00baa3988e1b998c497e497437e55
Roman-G-579 <romangury579@gmail.com>
2024-12-13 13:55:18 +0200

    Updated LibraryItem fields

--- film_planet-frontend/src/app/core/interfaces/library-item.interface.ts
+++ film_planet-frontend/src/app/core/interfaces/library-item.interface.ts
@@ -4,14 +4,21 @@ export interface LibraryItem {
   id: number;
   mediaType: MediaType;
   title: string;
-  release_date: Date | number;
-  endYear?: Date | number; // for tv shows
+  adult?: boolean;
+  video?: boolean;
+  release_date: string;
+  endYear?: string; // for tv shows
   duration?: number;
   overview?: string;
   genre_ids: number[];
   starring?: string[];
   directors?: string[];
   creators?: string[];
+  original_language?: string;
+  original_title?: string;
+  popularity?: number;
   poster_path?: string;
+  backdrop_path?: string;
   vote_average?: number;
+  vote_count?: number;
 }
