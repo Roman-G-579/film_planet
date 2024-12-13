8441a5abe676d91fcbe9e02faf0a99a587145eb6
Roman-G-579 <romangury579@gmail.com>
2024-12-13 13:54:07 +0200

    Updated api configuration details files' locations and structure

--- film_planet-frontend/src/app/core/config/api-details.ts
+++ film_planet-frontend/src/app/core/config/api-details.ts
@@ -1,7 +1,7 @@
 /**
  * Contains configuration details of the TMDB API
  */
-export const api_details = {
+export const API_DETAILS = {
     "change_keys": [
     "adult",
     "air_date",
@@ -57,44 +57,5 @@ export const api_details = {
     "video",
     "videos"
     ],
-    "images": {
-        "base_url": "http://image.tmdb.org/t/p/",
-            "secure_base_url": "https://image.tmdb.org/t/p/",
-            "backdrop_sizes": [
-            "w300",
-            "w780",
-            "w1280",
-            "original"
-        ],
-            "logo_sizes": [
-            "w45",
-            "w92",
-            "w154",
-            "w185",
-            "w300",
-            "w500",
-            "original"
-        ],
-            "poster_sizes": [
-            "w92",
-            "w154",
-            "w185",
-            "w342",
-            "w500",
-            "w780",
-            "original"
-        ],
-            "profile_sizes": [
-            "w45",
-            "w185",
-            "h632",
-            "original"
-        ],
-            "still_sizes": [
-            "w92",
-            "w185",
-            "w300",
-            "original"
-        ]
-    }
+    "base_url": "https://image.tmdb.org/t/p/",
 }
