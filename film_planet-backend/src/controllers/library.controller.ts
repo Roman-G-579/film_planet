d9b5609c31f3efa2539b68ea38623d4d36b0805d
Roman-G-579 <romangury579@gmail.com>
2024-12-16 20:32:38 +0200

    Refined library functions for better results

--- film_planet-backend/src/controllers/library.controller.ts
+++ film_planet-backend/src/controllers/library.controller.ts
@@ -1,6 +1,7 @@
 import {Config} from "../config/config";
 import {NextFunction, Request, Response} from "express";
 import httpStatus from "http-status";
+import {formatDate} from "../utils/data-utils";
 
 /**
  * Contains function related to the retrieval of tmdb items (films, tv shows, actors etc.)
@@ -14,53 +15,19 @@ const options = {
     }
 };
 
-// export async function getItemsFromApi(req: Request, res: Response, next: NextFunction) {
-//     try {
-//         const {mediaType, category} = req.params;
-//
-//         if (!mediaType || !['film', 'tv'].includes(mediaType)) {
-//             return res.status(httpStatus.BAD_REQUEST).send({error: 'Invalid media type in request header. Must be "film" or "tv".'});
-//         }
-//
-//         if (!category || !['recent', 'popular', 'top'].includes(category)) {
-//             return res.status(httpStatus.BAD_REQUEST).send({error: 'Invalid category in request header. Must be "recent", "popular", or "top".'});
-//         }
-//
-//         const {requestMediaType, requestCategory} = generateRequestStrings(mediaType, category);
-//
-//         const response = await fetch(`https://api.themoviedb.org/3/${requestMediaType}/${requestCategory}?language=en-US&page=1`, options);
-//         const data = await response.json();
-//
-//         return res.status(httpStatus.OK).send(data);
-//     } catch (err) {
-//         console.error(err);
-//         next(err);
-//     }
-// }
-
-// /**
-//  * Matches the mediaType and category strings to their equivalents that are used by the TMDB API requests
-//  * @param mediaType film or TV
-//  * @param category recent, popular or top
-//  */
-// function generateRequestStrings(mediaType: string, category: string): {requestMediaType: string, requestCategory: string} {
-//     // Matches the names provided by the header to the names used by the API requests
-//     const categoryMapping: Record<string, string> = {
-//         'film': 'movie',
-//         'film:recent': 'now_playing',
-//         'tv:recent': 'on_the_air',
-//     };
-//
-//     // Sets the correct strings with the help of the categoryMapping Record
-//     const requestMediaType = categoryMapping[`${mediaType}`] || mediaType;
-//     const requestCategory = categoryMapping[`${mediaType}:${category}`] || category;
-//
-//     return {requestMediaType, requestCategory};
-// }
-
 export async function getRecentFilmsMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
-        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options);
+        const currentDate = new Date();
+
+        // Minimum date - two weeks ago
+        const minDate = new Date(currentDate);
+        minDate.setDate(minDate.getDate() - 14);
+
+        // Maximum date - a week from now
+        const maxDate = new Date(currentDate);
+        maxDate.setDate(maxDate.getDate() + 7);
+
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&primary_release_date.gte.gte=${formatDate(minDate)}&primary_release_date.lte=${formatDate(maxDate)}&sort_by=popularity.desc&vote_count.gte=50`, options);
         const data = await response.json();
 
         return res.status(httpStatus.OK).send(data);
@@ -72,7 +39,15 @@ export async function getRecentFilmsMiddleware(req: Request, res: Response, next
 
 export async function getPopularFilmsMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
-        const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options);
+        const currentDate = new Date();
+        const minDate = new Date(currentDate);
+
+        // Sets the min date to the first of the month 3 months ago
+        minDate.setMonth(minDate.getMonth() - 3);
+        minDate.setDate(1);
+        const minDateStr = formatDate(minDate);
+
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&primary_release_date.gte=${minDateStr}&sort_by=vote_count.desc`, options);
         const data = await response.json();
 
         return res.status(httpStatus.OK).send(data);
@@ -94,7 +69,17 @@ export async function getTopFilmsMiddleware(req: Request, res: Response, next: N
 
 export async function getRecentTVMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
-        const response = await fetch('https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=1', options);
+        const currentDate = new Date();
+
+        // Minimum date - a week ago
+        const minDate = new Date(currentDate);
+        minDate.setDate(minDate.getDate() - 7);
+
+        // Maximum date - a week from now
+        const maxDate = new Date(currentDate);
+        maxDate.setDate(maxDate.getDate() + 7);
+
+        const response = await fetch(`https://api.themoviedb.org/3/discover/tv?air_date.gte=${formatDate(minDate)}&air_date.lte=${formatDate(maxDate)}language=en-US&page=1&sort_by=popularity.desc&vote_count.gte=50&with_original_language=en`, options);
         const data = await response.json();
 
         return res.status(httpStatus.OK).send(data);
@@ -108,11 +93,13 @@ export async function getPopularTVMiddleware(req: Request, res: Response, next: 
     try {
         const currentDate = new Date();
         const minDate = new Date(currentDate);
-        minDate.setMonth(minDate.getMonth() - 3);
-        minDate.setDate(1); // Sets the min date to the first of the month
-        const minDateStr = minDate.toISOString().split('T')[0];
 
-        const response = await fetch(`https://api.themoviedb.org/3/discover/tv?first_air_date.gte=${minDateStr}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=vote_count.desc&with_original_language=en`, options);
+        // Sets the min date to the first of the month 3 months ago
+        minDate.setMonth(minDate.getMonth() - 3);
+        minDate.setDate(1);
+        const minDateStr = formatDate(minDate);
+
+        const response = await fetch(`https://api.themoviedb.org/3/discover/tv?first_air_date.gte=${minDateStr}&language=en-US&page=1&sort_by=vote_count.desc&with_original_language=en`, options);
         const data = await response.json();
 
         return res.status(httpStatus.OK).send(data);
