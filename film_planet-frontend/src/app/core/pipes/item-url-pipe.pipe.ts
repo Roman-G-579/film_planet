31ac05f1dacf43ceb71a4d852d9073cb1e99edff
Roman-G-579 <romangury579@gmail.com>
2024-12-20 14:37:59 +0200

    Updated url pipe to support both film and TV show names

--- film_planet-frontend/src/app/core/pipes/item-url-pipe.pipe.ts
+++ film_planet-frontend/src/app/core/pipes/item-url-pipe.pipe.ts
@@ -13,12 +13,13 @@ import {LibraryItem} from '../interfaces/library-item.interface';
 export class ItemUrlPipePipe implements PipeTransform {
 
   transform(item: LibraryItem): string[] {
-    const itemName = `${item.id}-${item.title
+    const itemName = item.title || item.name || '';
+    const pageName = `${item.id}-${itemName
       .toLowerCase() // Convert to lowercase
       .replace(/[^a-z0-9\s-]/g, '') // Remove special characters except spaces and hyphens
       .replace(/\s+/g, '-')}`;
 
-    return ['/', 'pages', item.mediaType, itemName];
+    return ['/', 'pages', item.mediaType, pageName];
   }
 
 }
