caae91460ee9a6ba65ded7ab2258b82268486833
Roman-G-579 <romangury579@gmail.com>
2024-12-22 20:32:25 +0200

    Updated library-item field names

--- film_planet-frontend/src/app/core/interfaces/library-item.interface.ts
+++ film_planet-frontend/src/app/core/interfaces/library-item.interface.ts
@@ -1,6 +1,7 @@
 import {MediaType} from '../enums/media-type.enum';
 import {Credits} from './credits.interface';
 import {Genre} from './genre.interface';
+import {CastCrewMember} from './cast-crew-member.interface';
 
 /**
  * Interface for an individual film or tv show
@@ -13,13 +14,13 @@ export interface LibraryItem {
   budget?: number;
   release_date?: string; // used for films
   first_air_date?: string; // used for tv shows
-  endYear?: string; // for tv shows
+  last_air_date?: string;
   runtime?: number; // in minutes
   overview?: string;
   genres: Genre[];
   starring?: string[];
   directors?: string[];
-  created_by?: string[];
+  created_by?: CastCrewMember[];
   origin_country?: string;
   original_language?: string;
   original_title?: string; // used for films
