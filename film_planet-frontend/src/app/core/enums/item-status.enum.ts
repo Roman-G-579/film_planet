dcbf135f3525d77f74de85e55b89a4fcfebca50b
Roman-G-579 <romangury579@gmail.com>
2024-12-29 19:30:20 +0200

    Added enum for film and tv show statuses

---
+++ film_planet-frontend/src/app/core/enums/item-status.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * The possible statuses of the TMDB films and TV shows
+ */
+export enum ItemStatus {
+  Returning_Series = 'Returning Series',
+  Planned = 'Planned',
+  In_Production = 'In Production',
+  Ended = 'Ended',
+  Canceled = 'Canceled',
+  Pilot = 'Pilot',
+  Post_Production = 'Post Production',
+  Released = 'Released',
+  Rumored = 'Rumored',
+}
