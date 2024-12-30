51e2585d333c203af4331c8c51ebcbf7ad35e9c2
Roman-G-579 <romangury579@gmail.com>
2024-12-30 19:14:54 +0200

    Updated season interface's fields, and mock data to match them

--- film_planet-frontend/src/app/core/interfaces/season.interface.ts
+++ film_planet-frontend/src/app/core/interfaces/season.interface.ts
@@ -5,11 +5,11 @@ export interface Season {
   show_id?: number;
   id: number; // The season's TMDB id
   air_date?: string;
-  number?: number; // season 0 reserved for specials
   name: string; // series 1 etc. for uk shows, miniseries for miniseries, pilot for individual pilots
   overview?: string;
   poster_path?: string;
-  season_number?: string;
+  season_number: number;
+  episode_count?: number;
   vote_average?: number;
   episodes?: Episode[]; // Array of all episodes belonging to the season
 }
--- film_planet-frontend/src/app/core/mock-data/seasons.ts
+++ film_planet-frontend/src/app/core/mock-data/seasons.ts
@@ -4,37 +4,37 @@ export const SEASONS: Season[] = [
   {
     id: 1,
     show_id: 16,
-    number: 1,
+    season_number: 1,
     name: 'Season 1'
   },
   {
     id: 2,
     show_id: 16,
-    number: 2,
+    season_number: 2,
     name: 'Season 2'
   },
   {
     id: 3,
     show_id: 16,
-    number: 3,
+    season_number: 3,
     name: 'Season 3'
   },
   {
     id: 4,
     show_id: 16,
-    number: 4,
+    season_number: 4,
     name: 'Season 4'
   },
   {
     id: 5,
     show_id: 16,
-    number: 5,
+    season_number: 5,
     name: 'Season 5'
   },
   {
     id: 6,
     show_id: 17,
-    number: 1,
+    season_number: 1,
     name: 'Miniseries'
   }
 ]
