66259b492d4fbeb16e2e8f1e04bc5237e213de72
Roman-G-579 <romangury579@gmail.com>
2025-01-04 11:50:04 +0200

    -Updated search-as-you-type functionality -Updated layout

--- film_planet-frontend/src/app/pages/layout/search-window/search-window.component.ts
+++ film_planet-frontend/src/app/pages/layout/search-window/search-window.component.ts
@@ -1,9 +1,9 @@
 import {
   ChangeDetectionStrategy,
-  Component,
+  Component, computed,
   inject,
   model,
-  ModelSignal, OnInit,
+  ModelSignal, OnDestroy, OnInit,
   WritableSignal
 } from '@angular/core';
 import {ButtonModule} from "primeng/button";
@@ -11,13 +11,15 @@ import {DialogModule} from "primeng/dialog";
 import {LibraryService} from '../../../core/services/library.service';
 import {Router, RouterLink} from '@angular/router';
 import {DataViewModule} from 'primeng/dataview';
-import {DatePipe, DecimalPipe, NgClass, NgForOf} from '@angular/common';
+import {DatePipe, DecimalPipe, NgClass, NgForOf, NgIf} from '@angular/common';
 import {GenreNamesPipe} from '../../../core/pipes/genre-names.pipe';
 import {ItemUrlPipePipe} from '../../../core/pipes/item-url-pipe.pipe';
 import {PosterUrlPipePipe} from '../../../core/pipes/poster-url-pipe.pipe';
 import {LibraryItem} from '../../../core/interfaces/library-item.interface';
 import {DividerModule} from 'primeng/divider';
 import {MediaType} from '../../../core/enums/media-type.enum';
+import {debounceTime, distinctUntilChanged, Subject, takeUntil} from 'rxjs';
+import {MiscUtils} from '../../../core/utils/misc.utils';
 
 @Component({
   selector: 'app-search-window',
@@ -32,29 +34,68 @@ import {MediaType} from '../../../core/enums/media-type.enum';
     PosterUrlPipePipe,
     NgClass,
     DividerModule,
-    DatePipe
+    DatePipe,
+    NgIf
   ],
   templateUrl: './search-window.component.html',
   styleUrl: './search-window.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchWindowComponent {
+export class SearchWindowComponent implements OnInit, OnDestroy {
+  private searchSubject = new Subject<string>();
+  private destroy$ = new Subject<void>();
   protected readonly lib = inject(LibraryService);
+  protected readonly miscUtils = MiscUtils;
+  protected readonly MediaType = MediaType;
 
   isSearchVisible: ModelSignal<boolean> = model<boolean>(false);
 
   searchResults: WritableSignal<LibraryItem[]> = this.lib.libraryItems;
 
+  // The height of a row in the table (used for the virtualScroll functionality)
+  isSmallScreen = computed(() => {
+    const screenWidth = this.miscUtils.getScreenWidth();
+    return (screenWidth < 1024);
+  })
+
+  ngOnInit(): void {
+    // Subscribe to the search subject with debounce logic
+    this.searchSubject
+      .pipe(
+        debounceTime(300), // Wait 300ms after the user stops typing
+        distinctUntilChanged(), // Avoid duplicate searches for the same input
+        takeUntil(this.destroy$) // Unsubscribe when the component is destroyed
+      )
+      .subscribe((query) => {
+        this.searchItem(query);
+      });
+  }
+
+  /**
+   * Emits changes to the search subject when the search input value changes
+   * @param query the text in the input element
+   */
+  onSearchInputChange(query: string): void {
+    this.searchSubject.next(query);
+  }
+
+  /**
+   * Calls the search function to get new results matching the given query,
+   * after clearing the current search results
+   * @param query the search string, equal to the text in the input element
+   */
   searchItem(query: string) {
-    //TODO: handle presses of spaces etc.
     if (!query) {
       return;
     }
     // Clears the list of search results
     this.searchResults.set([]);
-    console.log(this.searchResults());
     this.lib.getSearchResultsFromApi(query);
   }
 
-  protected readonly MediaType = MediaType;
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 }
