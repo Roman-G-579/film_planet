1031b9cf777dff254eeda894054e7c8d0b13448e
Roman-G-579 <romangury579@gmail.com>
2025-01-08 20:19:37 +0200

    Added main cast component to item page

---
+++ film_planet-frontend/src/app/pages/item/cast-panel/cast-panel.component.ts
@@ -0,0 +1,67 @@
+import {ChangeDetectionStrategy, Component, computed, input, InputSignal, Signal} from '@angular/core';
+import {CarouselModule, CarouselResponsiveOptions} from 'primeng/carousel';
+import {CastCrewMember} from '../../../core/interfaces/cast-crew-member.interface';
+import {PosterUrlPipePipe} from '../../../core/pipes/poster-url-pipe.pipe';
+import {Credits} from '../../../core/interfaces/credits.interface';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-cast-panel',
+  standalone: true,
+  imports: [
+    CarouselModule,
+    PosterUrlPipePipe,
+    RouterLink
+  ],
+  templateUrl: './cast-panel.component.html',
+  styleUrl: './cast-panel.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CastPanelComponent {
+
+  credits: InputSignal<Credits> = input<Credits>({
+    id: 0,
+    cast: [],
+    crew: []
+  })
+
+  actors: InputSignal<CastCrewMember[]> = input<CastCrewMember[]>([]);
+
+  responsiveOptions: CarouselResponsiveOptions[] = [
+    {
+      breakpoint: '2100px',
+      numVisible: 8,
+      numScroll: 4,
+    },
+    {
+      breakpoint: '1800px',
+      numVisible: 7,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '1400px',
+      numVisible: 5,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '1199px',
+      numVisible: 5,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 4,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '575px',
+      numVisible: 3,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '400px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+  ];
+}
