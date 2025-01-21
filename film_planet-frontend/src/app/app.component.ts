9c603901b26a063cdb598a5e79d170cab7c898bd
Roman-G-579 <romangury579@gmail.com>
2025-01-21 19:11:36 +0200

    Added conditional class based on app language for rtl language formatting

--- film_planet-frontend/src/app/app.component.ts
+++ film_planet-frontend/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, Inject, LOCALE_ID} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import {PrimeNGConfig} from 'primeng/api';
 import {Aura} from 'primeng/themes/aura';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,10 @@ import {Aura} from 'primeng/themes/aura';
 })
 export class AppComponent {
 
-  constructor(private config: PrimeNGConfig) {
+  constructor(
+    private config: PrimeNGConfig,
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(LOCALE_ID) private locale: string) {
     this.config.theme.set({
       preset: Aura,
       options: {
@@ -20,8 +24,21 @@ export class AppComponent {
         darkModeSelector: '.film_planet-dark',
         cssLayer: false
       }
-    })
+    });
+
+    this.setLanguageClass(this.locale);
   }
 
-
+  /**
+   * Sets a class based on the chosen language. Used to handle right-to-left languages like hebrew and arabic.
+   * @param locale the locale name
+   */
+  private setLanguageClass(locale: string): void {
+    const htmlElement = this.document.documentElement;
+    if (locale === 'he') {
+      htmlElement.classList.add('lang-he');
+    } else {
+      htmlElement.classList.remove('lang-he');
+    }
+  }
 }
